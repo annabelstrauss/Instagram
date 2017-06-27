eac3f29b92a8d5985181f794c177c51bfee7f56f
Annabel Strauss <annabelstrauss@fb.com>
2017-06-27 15:59:05 -0700

    user can post photos

--- Instagram/CaptureViewController.swift
+++ Instagram/CaptureViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
-class CaptureViewController: UIViewController {
-
+class CaptureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var captionTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +23,56 @@ class CaptureViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
+    /* 
+    * This is the delegate method
     */
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        // Do something with the images (based on your use case)
+        photoImageView.image = editedImage
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func choosePhoto() {
+        // Instantiate a UIImagePickerController
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        // Check that the camera is indeed supported on the device before trying to present it
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available 📸")
+            vc.sourceType = .camera
+        } else {
+            print("Camera 🚫 available so we will use photo library instead")
+            vc.sourceType = .photoLibrary
+        }
+        
+        // Present the camera or photo library
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    @IBAction func onTapPhotoOpener(_ sender: Any) {
+        choosePhoto()
+    }
+    
+    @IBAction func didPressShare(_ sender: Any) {
+        let caption = captionTextField.text
+        let image = photoImageView.image
+        Post.postUserImage(image: image, withCaption: caption) { (success: Bool, error: Error?) in
+            print("post was created!")
+            print(success)
+            self.tabBarController?.selectedIndex = 0 //move to Home once post is created
+        }
+        
+    }
+    
 
 }
--- Instagram/ProfileViewController.swift
+++ Instagram/ProfileViewController.swift
@@ -7,13 +7,17 @@
 //
 
 import UIKit
+import Parse
 
 class ProfileViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        nameLabel.text = PFUser.current()?.username
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +25,5 @@ class ProfileViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
